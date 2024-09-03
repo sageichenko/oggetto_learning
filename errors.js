ac7232ff3b66fab6a95ebf9af860281875c66758
Sofiya Tereschenko <stereschenko@oggettoweb.com>
2024-09-03 16:18:43 +0300

    Fix error after review
    - fix use arguments in functions
    - improve the order of checks
    - remove catch ArgumentCountError

--- errors.js
+++ errors.js
@@ -13,17 +13,14 @@ class CustomError extends Error {
 }
 
 class ArgumentTypeError extends CustomError {}
-class ArgumentCountError extends CustomError {}
 
-const multiply = (number1, number2) => {
-    if ([...arguments].some((arg) => typeof arg !== 'number')) {
-        throw new ArgumentTypeError('Внимание! Один из аргументов метода не число');
+function multiply(number1, number2) {
+    if (arguments.length > 2) {
+        console.warn('Внимание! Передано больше указанного кол-ва аргументов')
     }
 
-    if (arguments.length > 2) {
-        // Я не понял, как выбросить предупреждение, но продолжить выполнение,
-        // т.к. после throw выполнение функции дальше не идет, поэтому оставила только console.warn
-        console.warn('Внимание! Передано больше указанного кол-ва аргументов')
+    if ([...arguments].some((arg) => typeof arg !== 'number')) {
+        throw new ArgumentTypeError('Внимание! Один из аргументов метода не число');
     }
 
     return number1 * number2;
@@ -35,19 +32,16 @@ const multiply = (number1, number2) => {
 * Ошибки выводить ввиде alert, предупреждения выводить в консоли.
 * */
 
-const executeSomeHardMultiply = (...arguments) => {
+function executeSomeHardMultiply (...arguments) {
     try {
         return multiply(...arguments);
     } catch (error) {
-        if (error instanceof ArgumentCountError) {
-            // Тут тоже не очень понятно, если мы попали в catch, значит выполнение прервалось,
-            // получается нужно еще раз вызвать multiply с правильным количеством аргументов?
-            // Сейчас мы в catch с ArgumentCountError не попадем, потому что внутри multiply не выбрасывается исключение
-            console.warn(error.message)
-
-            return multiply(arguments[0], arguments[1]);
-        } else if (error instanceof ArgumentTypeError) {
+        if (error instanceof ArgumentTypeError) {
             alert(error.message)
+
+            return;
         }
+
+        throw error;
     }
 }
